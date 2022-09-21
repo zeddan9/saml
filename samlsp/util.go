c9aad0e76add07a0a16ae046c09268ee47f54360
bdang <bdang@fortinet.com>
2022-09-20 22:59:05 -0700

    add samlsp back

---
+++ samlsp/util.go
@@ -0,0 +1,16 @@
+package samlsp
+
+import (
+	"io"
+
+	"github.com/zeddan9/saml"
+)
+
+func randomBytes(n int) []byte {
+	rv := make([]byte, n)
+
+	if _, err := io.ReadFull(saml.RandReader, rv); err != nil {
+		panic(err)
+	}
+	return rv
+}
